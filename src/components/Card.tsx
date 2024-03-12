59c5b641535d5920d341964ecec7bc63468f0059
Caleb Hypes <hello@calebhypes.com>
2024-03-11 23:32:03 -0400

    Added sortablecontext for hand, setup sortablecontext for answer slot

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React from "react";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 type Props = {
-    type: string,
-    text: string
+  type: string;
+  text: string;
+  id: number;
 };
 
 export default function Card(props: Props) {
-     const styles = props.type === "black" ? "bg-black text-white" : "bg-white text-black";
-    return (
-        <div className={`rounded text-left text-wrap ${styles} text-base w-48 h-64 px-2.5 pt-2.5 shadow-lg`}>{props.text}</div>
-    )
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: props.id });
+  const styles =
+    props.type === "black" ? "bg-black text-white" : "bg-white text-black";
+  const dndStyle = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+  return (
+    <div
+      className={`rounded text-left text-wrap ${styles} text-base w-48 h-64 px-2.5 pt-2.5 shadow-lg`}
+      ref={setNodeRef}
+      style={dndStyle}
+      {...attributes}
+      {...listeners}
+    >
+      {props.text}
+    </div>
+  );
 }
--- src/components/PlayArea.tsx
+++ src/components/PlayArea.tsx
@@ -1,77 +1,127 @@
-'use client'
-import React, {useState} from 'react';
-import { DndContext } from '@dnd-kit/core';
+"use client";
+import React, { useState } from "react";
+import {
+  DndContext,
+  closestCenter,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
 
-import {Droppable} from './Droppable';
-import {Draggable} from './Draggable';
+import {
+  arrayMove,
+  SortableContext,
+  //   horizontalListSortingStrategy,
+  rectSortingStrategy,
+} from "@dnd-kit/sortable";
 
-import Card from './Card';
+import Card from "./Card";
 
 function PlayArea() {
-    const testHand = [
-        {
-            type: "white",
-            text: "A PowerPoint Presentation."
-        },
-        {
-            type: "white",
-            text: "Crippling debt."
-        },
-        {
-            type: "white",
-            text: "Overthrowing the government."
-        },
-        {
-            type: "white",
-            text: "A box that is concious and wishes it weren't a box."
-        },
-        {
-            type: "white",
-            text: "A disappointing birthday party."
-        },
-        {
-            type: "white",
-            text: "An emotionally draining friendship"
-        },
-        {
-            type: "white",
-            text: "Meatloaf, the food."
-        }
-    ]
+  const testHand = [
+    {
+      type: "white",
+      text: "A PowerPoint Presentation.",
+      id: 1,
+    },
+    {
+      type: "white",
+      text: "Crippling debt.",
+      id: 2,
+    },
+    {
+      type: "white",
+      text: "Overthrowing the government.",
+      id: 3,
+    },
+    {
+      type: "white",
+      text: "A box that is concious and wishes it weren't a box.",
+      id: 4,
+    },
+    {
+      type: "white",
+      text: "A disappointing birthday party.",
+      id: 5,
+    },
+    {
+      type: "white",
+      text: "An emotionally draining friendship",
+      id: 6,
+    },
+    {
+      type: "white",
+      text: "Meatloaf, the food.",
+      id: 7,
+    },
+  ];
 
-    const testQuestion = {
-        type: "black",
-        text: "Hey baby, come back to my place and I'll show you __________."
-    };
+  const testQuestion = {
+    type: "black",
+    text: "Hey baby, come back to my place and I'll show you __________.",
+    id: 8,
+  };
 
-    const containers = ['A', 'B', 'C'];
-    const [parent, setParent] = useState(null);
-    const draggableMarkup = (
-        <Draggable id="draggable">Drag Me</Draggable>
-    )
-    
-    return (
-        <DndContext onDragEnd={handleDragEnd}> 
-            <div className="flex flex-col justify-center items-center gap-5">
-                <Card type={testQuestion.type} text={testQuestion.text} />
-                <div className="flex flex-row gap-x-2">
-                    {testHand.map((card, idx) => (
-                        <Card key={idx} type={card.type} text={card.text} />
-                    ))}
-                </div>
-            </div>
-            {parent === null ? draggableMarkup : null}
-            {containers.map((id) => (
-                <Droppable key={id} id={id}>
-                    {parent === id ? draggableMarkup : 'Drop here'}
-                </Droppable>
+  const [hand, setHand] = useState(testHand);
+
+  const sensors = useSensors(useSensor(PointerSensor));
+
+  // const draggableMarkup = (
+  //     <Draggable id="draggable">Drag Me</Draggable>
+  // )
+
+  return (
+    <DndContext
+      onDragEnd={handleDragEnd}
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragOver={handleDragOver}
+    >
+      <div className="flex flex-col justify-center items-center gap-5">
+        <Card
+          id={testQuestion.id}
+          type={testQuestion.type}
+          text={testQuestion.text}
+        />
+        <SortableContext items={[]} strategy={rectSortingStrategy}>
+          <div className="rounded flex justify-center items-center bg-gradient-to-t from-slate-500 text-slate-50 text-base w-52 h-72 shadow-lg">
+            Answer Card
+          </div>
+        </SortableContext>
+        <SortableContext items={hand} strategy={rectSortingStrategy}>
+          <div className="flex flex-row gap-x-2">
+            {hand.map((card) => (
+              <Card
+                key={card.id}
+                id={card.id}
+                type={card.type}
+                text={card.text}
+              />
             ))}
-        </DndContext>
-    );
-    function handleDragEnd(event) {
-        const {over} = event;
-        setParent(over ? over.id : null);
+          </div>
+        </SortableContext>
+      </div>
+    </DndContext>
+  );
+
+  function handleDragOver(event) {
+    // handle onDragOver
+  }
+
+  function handleDragEnd(event) {
+    const { active, over } = event;
+
+    console.log(active);
+    console.log(over);
+    if (active.id !== over.id) {
+      setHand((hand) => {
+        const oldIndex = hand.findIndex((card) => card.id === active.id);
+        const newIndex = hand.findIndex((card) => card.id === over.id);
+
+        return arrayMove(hand, oldIndex, newIndex);
+      });
     }
+  }
 }
 
 export default PlayArea;
